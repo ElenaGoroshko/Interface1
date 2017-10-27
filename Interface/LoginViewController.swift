bf4ece309fcafd4477a7571088e0577468696acb
ElenaGoroshko <32428178+ElenaGoroshko@users.noreply.github.com>
2017-10-27 23:16:55 +0300

    no message

--- Interface/LoginViewController.swift
+++ Interface/LoginViewController.swift
@@ -18,18 +18,11 @@ class LoginViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func backToLoginScreen (_ sender : UIStoryboardSegue ){
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
